18d1411029a6c6b85465c524d2135ce6d1498bbc
Matthew Bonig <matthew.bonig@gmail.com>
2020-09-06 16:31:52 -0600

    Initial commit

---
+++ bin/universal-nodejs-function.ts
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from '@aws-cdk/core';
+import { UniversalNodejsFunctionStack } from '../lib/universal-nodejs-function-stack';
+
+const app = new cdk.App();
+new UniversalNodejsFunctionStack(app, 'UniversalNodejsFunctionStack');
